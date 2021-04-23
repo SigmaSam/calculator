bc4daca3611aec0330e4c6d873878662f08887e9
samuel <samuelalmaoherrera@gmail.com>
2021-04-23 00:37:54 -0300

    Add some components.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
 
 function App() {
   return (
     <div className="App">
       <h1>Calculator</h1>
+      <Display />
+      <ButtonPanel />
     </div>
   );
 }
