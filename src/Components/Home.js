afc2d6c247d1f31e485ef13231e7247ce482be48
samuel <samuelalmaoherrera@gmail.com>
2021-05-18 23:01:56 -0300

    Add routing and basic CSS.

---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+const Home = () => (
+  <div>
+    <div>
+      <h1>Home</h1>
+      <p>
+        Here you can do Basic Math.
+        {'\n'}
+        For those times where you forget that almost all OS come with an integrated Calculator.
+      </p>
+
+    </div>
+  </div>
+);
+
+export default Home;
